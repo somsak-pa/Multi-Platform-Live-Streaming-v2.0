ed384c213852777b0fb30ad98ecc81743b0e1617
somsak-pa <65267176+somsak-pa@users.noreply.github.com>
2025-08-07 20:48:06 +0700

    Update index.js

--- src-tauri/node/config/index.js
+++ src-tauri/node/config/index.js
@@ -6,6 +6,6 @@ module.exports = {
   GOOGLE_CLIENT_SECRET: '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',
   GOOGLE_REDIRECT_URI: 'https://18d6267f3fdd.ngrok-free.app/api/auth/oauth2callback',
   // ************* เพิ่มบรรทัดนี้ *************
-  YOUTUBE_REFRESH_TOKEN: '1//0gPmGV3YpsaYJCgYIARAAGBASNwF-L9IrF2_pFn_19jWFVvTJJo5BHfX2n079zMqamD8_1OfamJjAKF7IlP7Yq-C2MrRNqpPqsX4', // <--- วาง Refresh Token จริงของคุณที่นี่
+  YOUTUBE_REFRESH_TOKEN: '', // <--- วาง Refresh Token จริงของคุณที่นี่
   // ****************************************
 };
